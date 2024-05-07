26b1e7096a1842829ee5892b3fca725954918ee5
CaQtiml <lsivakorn@gmail.com>
2024-05-07 09:26:39 +0700

    chore: Update DigitalOcean resources and configurations

--- terraform/09-digitalocean.tf
+++ terraform/09-digitalocean.tf
@@ -14,48 +14,48 @@ provider "digitalocean" {
 # try playing and see template here in /v2/apps
 # https://docs.digitalocean.com/reference/api/api-try-it-now/
 
-# resource "digitalocean_app" "bento_app" {
-#   spec {
-#     name   = "bento-app-spec-terraform"
-#     region = "sgp"
+resource "digitalocean_app" "bento_app" {
+  spec {
+    name   = "bento-app-spec-terraform"
+    region = "sgp"
 
-#     env {
-#       key   = "AWS_ACCESS_KEY_ID"
-#       value = var.s3_access
-#       type  = "SECRET"
-#     }
-#     env {
-#       key   = "AWS_SECRET_ACCESS_KEY"
-#       value = var.s3_secret_access
-#       type  = "SECRET"
-#     }
-#     env {
-#       key   = "URL"
-#       value = var.mlflow_url
-#       type  = "GENERAL"
-#     }
+    env {
+      key   = "AWS_ACCESS_KEY_ID"
+      value = var.s3_access
+      type  = "SECRET"
+    }
+    env {
+      key   = "AWS_SECRET_ACCESS_KEY"
+      value = var.s3_secret_access
+      type  = "SECRET"
+    }
+    env {
+      key   = "URL"
+      value = var.mlflow_url
+      type  = "GENERAL"
+    }
 
-#     alert {
-#       rule = "DEPLOYMENT_FAILED"
-#     }
+    alert {
+      rule = "DEPLOYMENT_FAILED"
+    }
 
-#     service {
-#       name = "bento-service-image"
-#       image {
-#         registry_type = "DOCR"
-#         repository    = "c6prhswwdw76lc4z"
-#         tag           = "latest"
-#         deploy_on_push {
-#           enabled = true
-#         }
-#       }
+    service {
+      name = "bento-service-image"
+      image {
+        registry_type = "DOCR"
+        repository    = "c6prhswwdw76lc4z"
+        tag           = "latest"
+        deploy_on_push {
+          enabled = true
+        }
+      }
 
-#       http_port          = 8080
-#       instance_size_slug = "basic-s"
-#       instance_count     = 1
-#     }
-#   }
-# }
+      http_port          = 8080
+      instance_size_slug = "basic-m"
+      instance_count     = 1
+    }
+  }
+}
 
 # resource "digitalocean_droplet" "miscellaneous_instance" {
 #   image      = "ubuntu-23-10-x64"
