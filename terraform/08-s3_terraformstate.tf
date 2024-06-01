d5e2da55eba29622a0c79e2f11aba45b324f0cfe
CaQtiml <lsivakorn@gmail.com>
2024-06-01 08:41:36 +0700

    Update DigitalOcean resources and configurations

--- terraform/08-s3_terraformstate.tf
+++ terraform/08-s3_terraformstate.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "seniorproj-terraform-state-s3"
   # Prevent accidental deletion of this S3 bucket
-  lifecycle {
-    prevent_destroy = true
-  }
+  # lifecycle {
+  #   prevent_destroy = true
+  # }
 }
 
 resource "aws_s3_bucket_versioning" "enabled" {
@@ -40,12 +40,12 @@ resource "aws_dynamodb_table" "terraform_locks" {
   }
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "seniorproj-terraform-state-s3"
-    key            = "global/s3/terraform.tfstate"
-    region         = "ap-southeast-1"
-    dynamodb_table = "seniorproj-terraform-state-dynamodb"
-    encrypt        = true
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "seniorproj-terraform-state-s3"
+#     key            = "global/s3/terraform.tfstate"
+#     region         = "ap-southeast-1"
+#     dynamodb_table = "seniorproj-terraform-state-dynamodb"
+#     encrypt        = true
+#   }
+# }
